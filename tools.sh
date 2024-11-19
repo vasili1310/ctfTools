2951617647a17809018be9fc0761cabf851f771a
vasili1310 <95551096+vasili1310@users.noreply.github.com>
2024-11-19 17:20:08 +0600

    Update tools.sh

--- tools.sh
+++ tools.sh
@@ -7,6 +7,7 @@ tools = (
 	"git"
 	"python3"
 	"python3-pip"
+	"gdb"
 )
 
 # check for sudo
@@ -14,6 +15,9 @@ if [ "$(id -u)" -ne 0 ]; then
 	echo "This script requires sudo privileges :("
 	exit 1
 fi
+
+mkdir CTFTools
+cd CTFTools
 
 install_debian() {
 	echo "Installing tools for Debian-based system"
@@ -67,16 +71,109 @@ gitHub_tools() {
 	cd ..
 
 	# apktool
-	https://github.com/iBotPeaches/Apktool
+	git clone https://github.com/iBotPeaches/Apktool
 	# should add more here
 	
 	# barf
 	# should add more here
 	
+	# binary ninja
+	# add more here
+
+	# binutils
+	# add more here
+
+	# boomerang
+	# add more here
+
+	# ctf_import
+	git clone https://github.com/sciencemanx/ctf_import
+
+	# cwe_checker
+	docker build -t cwe_checker .
+
+	# demovfuscator
+	# work in progress, so skipping
+
+	# frida
+	pip install frida-tools
+	pip install frida
+	npm install frida
+
+	# GEF
+	bash -c "$(curl -fsSL https://gef.blah.cat/sh)"
+
+	# Hopper
+	git clone https://github.com/FuzzAnything/Hopper
+	cd Hopper
+	./build.sh
+	cd ..
+
+	# Jadx
+	# add more here
+
+	# Java decompilers
+	git clone https://github.com/java-decompiler/jd-gui.git
+	cd jd-gui
+	./gradlew build
+	cd ..
+
+	# Krakatau
+	git clone https://github.com/Storyyeller/Krakatau.git
+	cd Krakatau
+	cargo build --release
+	cd ..
+
+	# objection
+	pip3 install objection
+
+	# PEDA
+	# add more
+
+	# pin
+	# add more
+
+	# PINCE
+	# download the release from github
+
+	# PinCTF
+	git clone https://github.com/ChrisTheCoolHut/PinCTF
+	cd PinCTF
+	./installPin.sh
+	cd ..
+
+	# Plasma
+	# add more
+
+	
+}
+
+other_tools() {
+	echo "Installing other useful programs ..."
+
+	# docker
+	for pkg in docker.io docker-doc docker-compose docker-compose-v2 podman-docker containerd runc; do sudo apt-get remove $pkg; done
+	# Add Docker's official GPG key:
+	apt-get update
+	apt-get install ca-certificates curl
+	install -m 0755 -d /etc/apt/keyrings
+	curl -fsSL https://download.docker.com/linux/ubuntu/gpg -o /etc/apt/keyrings/docker.asc
+	chmod a+r /etc/apt/keyrings/docker.asc
+
+	# Add the repository to Apt sources:
+	echo \
+  	"deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.asc] https://download.docker.com/linux/ubuntu \
+  	$(. /etc/os-release && echo "$VERSION_CODENAME") stable" | \
+  	 tee /etc/apt/sources.list.d/docker.list > /dev/null
+	apt-get update
+
+	apt-get install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
 	
 }
 
 install_debian
 
+other_tools
 
+gitHub_tools
 
